26c546428d0385e2b0012250e8c039754d0ce5f3
crichterdev <carlos.richter.dev@outlook.com>
2023-07-24 23:17:59 -0400

    Event binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,20 +5,36 @@ import { Component } from "@angular/core";
   styleUrls: ["app.component.scss"],
   template: `
     <div class="app">
-      <h1>{{ title }}</h1>
-      <h1 [innerHTML]="title"></h1>
-
-      <img [src]="logo">
-
-      <input type="text" [value]="name">
+      <button  (click)="handleClick()">button</button>
+      <input
+        [value]="name"
+        (input)="handleInput($event)"
+        (blur)="handleBlur($event)"
+      />
+      <div>{{ name }}</div>
     </div>
-  `
+  `,
 })
 export class AppComponent {
   title: string;
-  logo:string='img/logo.svg';
-  name:string= 'Todd';
+  logo: string = "img/logo.svg";
+  name: string = "Todd";
 
+  handleBlur(event: any) {
+    // this will update the property binding name.
+    this.name = event.target.value;
+    console.log(event);
+  }
+
+  handleInput(event: Event) {
+    //from my own research de expected type instead of any.
+    this.name = (event.target as HTMLInputElement).value;
+    console.log(event);
+  }
+
+  handleClick() {
+    this.name = "Todd";
+  }
   constructor() {
     this.title = "Ultimate Angular";
   }
